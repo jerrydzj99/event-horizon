6d3cf4a2632fcd4ee6d3732584e40144d8df49e0
xinyun-c <xinyun_c@berkeley.edu>
2020-02-20 22:31:53 -0800

    edit playerController

--- Event Horizon VR/Assets/Scripts/PlayerController.cs
+++ Event Horizon VR/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // ***** TODO: GLOBAL VARIABLES *****
-
+    public MainController control;
+    public GameObject currentRoom;
+    public GameObject player;
     // ***** TODO: PRIVATE VARIABLES *****
 
     // Start is called before the first frame update
@@ -23,7 +25,22 @@ public class PlayerController : MonoBehaviour
     // ***** TODO: TRIGGER MOVEMENT *****
 
     void OnTriggerStay(Collider other) {
-
+        // The Direction and UP, DOWN, LEFT and RIGHT are enums declared in DataController
+        if (other.GetComponent<DataController.Direction>() == DataController.Direction.Up) {
+            control.PlayerMoveUp();
+        }
+        if (other.GetComponent<DataController.Direction>() == DataController.Direction.Down)
+        {
+            control.PlayerMoveDown();
+        }
+        if (other.GetComponent<DataController.Direction>() == DataController.Direction.Left)
+        {
+            control.PlayerMoveLeft();
+        }
+        if (other.GetComponent<DataController.Direction>() == DataController.Direction.Right)
+        {
+            control.PlayerMoveRight();
+        }
     }
 
     // ***** TODO: UPDATE PLAYER POSITION *****
@@ -31,6 +48,7 @@ public class PlayerController : MonoBehaviour
     // Move the player to the room passed in as an argument and set the localTransform
     public void UpdatePlayerPosition(GameObject room, Transform localTransform)
     {
-
+        currentRoom = room;
+        player.transform.position = localTransform.position;
     }
 }
