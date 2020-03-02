dab1bc61c453bc2e39c1859651a5ea0916863ca2
sphindle1 <will14709@gmail.com>
2020-03-01 16:50:23 -0800

    main controller p1

--- Event Horizon VR/Assets/Scripts/MainController.cs
+++ Event Horizon VR/Assets/Scripts/MainController.cs
@@ -13,6 +13,9 @@ public class MainController : MonoBehaviour
     public GameObject intersection;
 
     // ***** TODO: PRIVATE VARIABLES *****
+    private int[][] mapArray;
+    private int playerX;
+    private int playerY;
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +38,62 @@ public class MainController : MonoBehaviour
     public void MovePlayer(DataController.Direction direction) {
         // Compute the destination room and the localTransform relative to the new room (this depends on the way from which the player enters the room) from direction and mapArray
         // Call playerController.UpdatePlayerPosition(GameObject room, Transform localTransform)
+        int newX = playerX;
+        int newY = playerY;
+        switch (direction)
+        {
+            case Up:
+                newY = playerY + 1;
+            case Down:
+                newY = playerY - 1;
+            case Left:
+                newX = playerX - 1;
+            case Right:
+                newx = playerX + 1;
+        }
+
+        int numDoors = 0;
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+        if (newX + 1 < mapArray.Length() && mapArray[newX + 1][newY])
+        {
+            numDoors += 1;
+            up = true;
+        }
+        if (newX - 1 > 0 && mapArray[newX - 1][newY])
+        {
+            numDoors += 1;
+            down = true;
+        }
+        if (newY + 1 < mapArray[0].Length() && mapArray[newX][newY + 1])
+        {
+            numDoors += 1;
+            right = true;
+        }
+        if (newY - 1 > 0 && mapArray[newX][newY - 1])
+        {
+            numDoors += 1;
+            left = true;
+        }
+
+        GameObject room = corner;
+        switch(numDoors)
+        {
+            case 1:
+                room = deadend;
+            case 2:
+                if ((left && right) || (up && down) {
+                    room = hallway;
+                }
+            case 3:
+                room = tshaped;
+            case 4:
+                room = intersection;
+        }
+
+        //TODO calculate local transform, and pass it into update player position
     }
 
 
